d95c2436019de6962ba4ee3cc2939274cda0ff94
AnTiZ <kai.hueet@posteo.de>
2018-01-12 16:35:31 +0100

    add comment that was forgotten

--- src/iroffer_defines.h
+++ src/iroffer_defines.h
@@ -249,4 +249,4 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #error "** ERROR: This OS Is Not Supported Or You Didn't Run Configure **"
 #endif
 
-#endif _IROFFER_DEFINES
+#endif /* _IROFFER_DEFINES */

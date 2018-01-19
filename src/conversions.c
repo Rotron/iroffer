ad9c4ac19bd733ec513fa309b83da47947ee3526
AnTiZ <kai.hueet@posteo.de>
2018-01-19 17:58:08 +0100

    use extra const in function definitions shorter file header-comment start using doxygen

--- src/conversions.c
+++ src/conversions.c
@@ -1,27 +1,17 @@
-/*
-iroffer - An IRC file server using the DCC protocol
-Copyright (C) see CONTRIBUTORS
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
+/**
+ * Implementation of conversion functions
+ * @file
+ * @copyright see CONTRIBUTORS
+ * @license
+ * This file is licensed under the GPLv3+ as found in the LICENSE file.
+ */
 
 #include "conversions.h"
 
-#include "string.h"
+#include <string.h>
 
 
-unsigned long atoul(const char* str) {
+unsigned long atoul(const char* const str) {
     unsigned long num, temp;
     int i, j;
     if (str == NULL) {
@@ -42,7 +32,7 @@ unsigned long atoul(const char* str) {
 
 
 // TODO(AnTiZ): evaluate replacement with stdint types
-unsigned long long atoull(const char* str) {
+unsigned long long atoull(const char* const str) {
     unsigned long long num, temp;
     int i, j;
     if (str == NULL) {
--- src/conversions.h
+++ src/conversions.h
@@ -1,25 +1,28 @@
-/*
-iroffer - An IRC file server using the DCC protocol
-Copyright (C) see CONTRIBUTORS
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
+/**
+ * Declaration of conversion functions
+ * @file
+ * @copyright see CONTRIBUTORS
+ * @license
+ * This file is licensed under the GPLv3+ as found in the LICENSE file.
+ */
 
 #ifndef IROFFER_CONVERSIONS_H
 #define IROFFER_CONVERSIONS_H
 
+/**
+ * Convert a string of digits into an unsigned long
+ *
+ * @param str String to be converted; may be NULL.
+ * @return The converted number; 0 if str is NULL.
+ */
 unsigned long atoul(const char* str);
+
+/**
+ * Convert a string of digits into an unsigned long long
+ *
+ * @param str String to be converted; may be NULL.
+ * @return The converted number; 0 if str is NULL.
+ */
 unsigned long long atoull(const char* str);
 
 #endif // IROFFER_CONVERSIONS_H

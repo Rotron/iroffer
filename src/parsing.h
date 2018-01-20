7989bd9385be0728da3fd284290cdbb05c0a3560
AnTiZ <kai.hueet@posteo.de>
2018-01-20 19:19:52 +0100

    move privmsgparse to separate file

---
+++ src/parsing.h
@@ -0,0 +1,14 @@
+/**
+ * Declaration of parsing functions
+ * @file
+ * @copyright see CONTRIBUTORS
+ * @license
+ * This file is licensed under the GPLv3+ as found in the LICENSE file.
+ */
+
+#ifndef IROFFER_PARSING_H
+#define IROFFER_PARSING_H
+
+void privmsgparse(const char* type, char* line);
+
+#endif // IROFFER_PARSING_H
